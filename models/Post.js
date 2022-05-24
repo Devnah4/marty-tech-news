9d5d5f90ce6ad12a580cffe5ca9c01de582a9d82
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-24 18:05:28 -0400

    Finished Models
    Sets code for index to import models as well as the models individual codes

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// Creates the Post model
+class Post extends Model {}
+
+// creates fields and columns for Post Model
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_text: {
+      // Sets the value to read text so it populates as text
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        isURL: true,
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      //   uses the user id value to use user info
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,34 @@
 const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
+
+// Creates table associations
+// Sets the relationship between Users and their posts
+User.hasMany(Post, {
+    foreignKey: "user_id",
+});
+
+// Sets posts and comments to only relate to one User
+Post.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+Comment.belongTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Sets comments relationship to a Post
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+});
+
+// Sets User and Post relationship to many Comment
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment};
