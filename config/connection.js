f9db424ffe7dabe02458702e30fd20c844329d30
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-24 14:25:26 -0400

    Create connection.js

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+// Sets up databse for use with mysql
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3301
+});
+
+module.exports = sequelize;
