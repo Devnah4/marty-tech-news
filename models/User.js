a6faad13be90b79ec7c1c0c679d09601c265f864
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-24 15:09:38 -0400

    Set up User info and added more framework

---
+++ models/User.js
@@ -0,0 +1,68 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+// Creates a hashed login
+const bcrypt = require("bcrypt");
+
+// Create the User Model
+class User extends Model {
+  // Checks to verify hashed password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+// Defines the User tables and columns
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+    // pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: "user",
+  }
+);
