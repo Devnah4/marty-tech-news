b7a4b1803922ab06efeafa3f74059ee49bd56a87
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-24 15:10:37 -0400

    env set up

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Sets up databse for use with mysql
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3301
