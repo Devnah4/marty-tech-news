2febc291266f67ae35490a9685291fb68fb5358a
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-28 02:09:16 -0400

    adjusted for DB usage

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// Sets up databse for use with mysql
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+const sequelize = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306
 });
